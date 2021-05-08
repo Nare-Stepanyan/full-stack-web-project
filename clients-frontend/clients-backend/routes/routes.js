06262b3d9a810fc59b8c3fa237b730a3c219d7b1
Nare-Stepanyan <narestepanyan90@gmail.com>
2021-05-08 16:08:56 +0300

    server configuration

---
+++ clients-frontend/clients-backend/routes/routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const clientTemplate = require("./../models/clients.schema");
+const providerTemplate = require("./../models/providers.schema");
+
+const router = express.Router();
+
+router.post("/clients", (req, res) => {
+  const client = new clientTemplate({
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    providers: req.body.providers,
+  });
+  client
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.post("/providers", (req, res) => {
+  const provider = new providerTemplate({
+    name: req.body.name,
+  });
+  provider
+    .save()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("./clientList", (req, res) => {
+  console.log(`Get client list`);
+});
+
+module.exports = router;
---
+++ clients-frontend/clients-backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const routesUrls = require("./routes/routes");
+const PORT = process.env.PORT || 3001;
+
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE_ACCESS, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("db connected!");
+});
+
+app.use(express.json());
+app.use(cors());
+app.use("/app", routesUrls);
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
